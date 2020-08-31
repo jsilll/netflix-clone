2837a771ff5e045c12db044de9fc67218612de2b
joao <joao20014@gmail.com>
2020-08-31 19:03:16 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix-clone/precache-manifest.e4e6bd811ef3bad25293ee36593e1bbf.js"
+  "/netflix-clone/precache-manifest.60be584347cf0ff407f3940ef8ec587c.js"
 );
 
 self.addEventListener('message', (event) => {
